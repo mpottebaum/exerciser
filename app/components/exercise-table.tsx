80abade22d0d7fd610364248bd2cd4bdea821df3
Mike Pottebaum <michael.pottebaum@cdw.com>
2023-11-12 12:30:57 -0600

    build: exercise-table

--- app/components/exercise-table.tsx
+++ app/components/exercise-table.tsx
@@ -1,4 +1,55 @@
+import { useEffect, useRef, useState } from 'react'
 import type { Exercise } from '~/types'
+
+interface EditableTextProps {
+  text: string | number
+  name: string
+  type: 'text' | 'number'
+  onSubmit: (value: string | number) => void
+}
+
+function EditableText({ text, name, type, onSubmit }: EditableTextProps) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [isEditing, setIsEditing] = useState(false)
+
+  const handleTextClick = (
+    e: React.MouseEvent<HTMLParagraphElement, MouseEvent>,
+  ) => {
+    setIsEditing(true)
+  }
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const inputValue = (
+      e.currentTarget.elements.namedItem(name) as HTMLInputElement
+    ).value
+    onSubmit(inputValue)
+    setIsEditing(false)
+  }
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.value = text.toString()
+      inputRef.current.focus()
+    }
+  }, [isEditing, text])
+  return (
+    <>
+      {!isEditing && <p onClick={handleTextClick}>{text}</p>}
+      {isEditing && (
+        <form onSubmit={handleFormSubmit}>
+          <input
+            ref={inputRef}
+            onBlur={() => setIsEditing(false)}
+            className="w-full"
+            type={type}
+            name={name}
+          />
+        </form>
+      )}
+    </>
+  )
+}
 
 export interface ExerciseTableProps<T extends Exercise> {
   exercises: T[]
@@ -27,7 +78,14 @@ export function ExerciseTable<T extends Exercise>({
               const datum = exercise[header]
               return typeof datum === 'string' || typeof datum === 'number' ? (
                 <td key={header} className="border-2 border-black p-2">
-                  {datum}
+                  <EditableText
+                    text={datum}
+                    name={header}
+                    type={typeof datum === 'string' ? 'text' : 'number'}
+                    onSubmit={(val: string | number) =>
+                      console.log('submitted', val)
+                    }
+                  />
                 </td>
               ) : null
             })}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,39 +1,5 @@
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-import { mockWorkoutRoutines } from '~/mock-data'
-// import { db, dbTables } from '~/db'
-import { WorkoutRoutine } from '~/components'
+import { redirect } from '@remix-run/node'
 
 export async function loader() {
-  // const result = await db.from(dbTables.workouts).select()
-  // return json(result.data)
-  const workoutRoutines = mockWorkoutRoutines
-  return json([
-    workoutRoutines[0],
-    {
-      ...workoutRoutines[0],
-      id: 2,
-    },
-  ])
-}
-
-export default function Index() {
-  const workouts = useLoaderData<typeof loader>()
-  return (
-    <div className="flex w-full flex-col items-center">
-      <ul>
-        {workouts?.map((workout) => (
-          <li key={workout.id}>
-            <div className="pb-4">
-              <WorkoutRoutine
-                workoutRoutine={workout}
-                variant="card"
-                linkTo={`/workout-routines/${workout.id}`}
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+  throw redirect('/workout-routines', 302)
 }
